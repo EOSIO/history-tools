f88b5677c1c99ca860c699ec8b1d2dd3be47a343
Todd Fleming <todd.fleming@block.one>
2018-12-05 14:45:24 -0500

    flexible query

--- src/init.sql
+++ src/init.sql
@@ -7,18 +7,29 @@ create index if not exists contract_row_code_table_primary_key_scope_block_index
     present desc
 );
 
-drop function if exists chain.contract_row_range_scope;
-create function chain.contract_row_range_scope(
+create index if not exists contract_row_code_table_scope_primary_key_block_index_prese_idx on chain.contract_row(
+    code,
+    "table",
+    scope,
+    primary_key,
+    block_index desc,
+    present desc
+);
+
+drop function if exists chain.contract_row_range_code_table_pk_scope;
+create function chain.contract_row_range_code_table_pk_scope(
     max_block_index bigint,
-    code varchar(13),
-    min_scope varchar(13),
-    max_scope varchar(13),
-    "table" varchar(13),
-    primary_key numeric,
+    first_code varchar(13),
+    first_table varchar(13),
+    first_primary_key numeric,
+    first_scope varchar(13),
+    last_code varchar(13),
+    last_table varchar(13),
+    last_primary_key numeric,
+    last_scope varchar(13),
     max_results integer
 ) returns setof chain.contract_row
 as $$
-    #variable_conflict use_variable
     declare
         key_search record;
         block_search record;
@@ -31,15 +42,11 @@ as $$
         end if;
         for key_search in
             select
-                contract_row.scope
+                code, "table", primary_key, scope
             from
                 chain.contract_row
             where
-                contract_row.code = code
-                and contract_row."table" = "table"
-                and contract_row.primary_key = primary_key
-                and contract_row.scope >= min_scope
-                and contract_row.scope <= max_scope
+                (code, "table", primary_key, scope) >= (first_code, first_table, first_primary_key, first_scope)
             order by
                 code,
                 "table",
@@ -49,18 +56,24 @@ as $$
                 present desc
             limit 1
         loop
+            if (key_search.code, key_search."table", key_search.primary_key, key_search.scope) > (last_code, last_table, last_primary_key, last_scope) then
+                return;
+            end if;
             found_key = true;
             found_block = false;
-            min_scope = key_search.scope;
+            first_code = key_search.code;
+            first_table = key_search."table";
+            first_primary_key = key_search.primary_key;
+            first_scope = key_search.scope;
             for block_search in
                 select
                     *
                 from
                     chain.contract_row
                 where
-                    contract_row.code = code
-                    and contract_row."table" = "table"
-                    and contract_row.primary_key = primary_key
+                    contract_row.code = key_search.code
+                    and contract_row."table" = key_search."table"
+                    and contract_row.primary_key = key_search.primary_key
                     and contract_row.scope = key_search.scope
                     and contract_row.block_index <= max_block_index
                 order by
@@ -79,7 +92,7 @@ as $$
                 end if;
             end loop;
             if not found_block then
-                return next row(0::bigint, false, code::varchar(13), key_search.scope::varchar(13), "table"::varchar(13), primary_key, ''::varchar(13), ''::bytea);
+                return next row(0::bigint, false, key_search.code::varchar(13), key_search.scope::varchar(13), key_search."table"::varchar(13), key_search.primary_key, ''::varchar(13), ''::bytea);
                 num_results = num_results + 1;
             end if;
         end loop;
@@ -90,15 +103,11 @@ as $$
             found_key = false;
             for key_search in
                 select
-                    contract_row.scope
+                    code, "table", primary_key, scope
                 from
                     chain.contract_row
                 where
-                    contract_row.code = code
-                    and contract_row."table" = "table"
-                    and contract_row.primary_key = primary_key
-                    and contract_row.scope > min_scope
-                    and contract_row.scope <= max_scope
+                    (code, "table", primary_key, scope) > (first_code, first_table, first_primary_key, first_scope)
                 order by
                     code,
                     "table",
@@ -108,18 +117,24 @@ as $$
                     present desc
                 limit 1
             loop
+                if (key_search.code, key_search."table", key_search.primary_key, key_search.scope) > (last_code, last_table, last_primary_key, last_scope) then
+                    return;
+                end if;
                 found_key = true;
                 found_block = false;
-                min_scope = key_search.scope;
+                first_code = key_search.code;
+                first_table = key_search."table";
+                first_primary_key = key_search.primary_key;
+                first_scope = key_search.scope;
                 for block_search in
                     select
                         *
                     from
                         chain.contract_row
                     where
-                        contract_row.code = code
-                        and contract_row."table" = "table"
-                        and contract_row.primary_key = primary_key
+                        contract_row.code = key_search.code
+                        and contract_row."table" = key_search."table"
+                        and contract_row.primary_key = key_search.primary_key
                         and contract_row.scope = key_search.scope
                         and contract_row.block_index <= max_block_index
                     order by
@@ -138,7 +153,7 @@ as $$
                     end if;
                 end loop;
                 if not found_block then
-                    return next row(0::bigint, false, code::varchar(13), key_search.scope::varchar(13), "table"::varchar(13), primary_key, ''::varchar(13), ''::bytea);
+                    return next row(0::bigint, false, key_search.code::varchar(13), key_search.scope::varchar(13), key_search."table"::varchar(13), key_search.primary_key, ''::varchar(13), ''::bytea);
                     num_results = num_results + 1;
                 end if;
             end loop;

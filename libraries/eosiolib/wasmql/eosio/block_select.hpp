909a48dc3f773210c3382bff1abf7176404d6a5f
Todd Fleming <todd.fleming@block.one>
2019-03-07 15:11:08 -0500

    doc

--- libraries/eosiolib/wasmql/eosio/block_select.hpp
+++ libraries/eosiolib/wasmql/eosio/block_select.hpp
@@ -3,6 +3,40 @@
 #pragma once
 #include <eosio/database.hpp>
 
+/// \file
+/// ```c++
+/// using absolute_block     = tagged_type<"absolute"_n, int32_t>;
+/// using head_block         = tagged_type<"head"_n, int32_t>;
+/// using irreversible_block = tagged_type<"irreversible"_n, int32_t>;
+///
+/// using block_select       = tagged_variant<
+///                                serialize_tag_as_index,
+///                                absolute_block,
+///                                head_block,
+///                                irreversible_block>;
+///
+/// block_select make_absolute_block(int32_t i);
+///
+/// uint32_t get_block_num(const block_select& sel, const database_status& status);
+/// ```
+/// `block_select` identifies blocks. This appears in JSON as one of the following:
+/// ```
+/// ["absolute", 1234]          Block 1234
+/// ["head", -10]               10 blocks behind head
+/// ["irreversible", -4]        4 blocks behind irreversible
+/// ```
+///
+/// ```c++
+/// block_select make_absolute_block(int32_t i);
+/// ```
+/// Returns a `block_select` which references block `i`
+///
+/// ```c++
+/// uint32_t get_block_num(const block_select& sel, const database_status& status);
+/// ```
+/// Returns the block that `sel` references.
+
+/// \exclude
 namespace eosio {
 
 using absolute_block     = tagged_type<"absolute"_n, int32_t>;
--- libraries/eosiolib/wasmql/eosio/input-output.hpp
+++ libraries/eosiolib/wasmql/eosio/input-output.hpp
@@ -2,12 +2,18 @@
 
 #pragma once
 
+#include <string_view>
+#include <vector>
+
 namespace eosio {
 
+/// \exclude
 typedef void* cb_alloc_fn(void* cb_alloc_data, size_t size);
 
+/// \exclude
 extern "C" void get_input_data(void* cb_alloc_data, cb_alloc_fn* cb_alloc);
 
+/// \exclude
 template <typename Alloc_fn>
 inline void get_input_data(Alloc_fn alloc_fn) {
     get_input_data(&alloc_fn, [](void* cb_alloc_data, size_t size) -> void* { //
@@ -15,6 +21,7 @@ inline void get_input_data(Alloc_fn alloc_fn) {
     });
 }
 
+/// Get data provided to the wasm
 inline std::vector<char> get_input_data() {
     std::vector<char> result;
     get_input_data([&result](size_t size) {
@@ -24,8 +31,15 @@ inline std::vector<char> get_input_data() {
     return result;
 }
 
-extern "C" void set_output_data(const char* begin, const char* end);
-inline void     set_output_data(const std::vector<char>& v) { set_output_data(v.data(), v.data() + v.size()); }
-inline void     set_output_data(const std::string_view& v) { set_output_data(v.data(), v.data() + v.size()); }
+extern "C" {
+/// Set the wasm's output data
+void set_output_data(const char* begin, const char* end);
+}
+
+/// Set the wasm's output data
+inline void set_output_data(const std::vector<char>& v) { set_output_data(v.data(), v.data() + v.size()); }
+
+/// Set the wasm's output data
+inline void set_output_data(const std::string_view& v) { set_output_data(v.data(), v.data() + v.size()); }
 
 } // namespace eosio

faffbc8ce7a1331cd5e578ff86e4d99a88232830
Todd Fleming <todd.fleming@block.one>
2020-02-11 15:14:29 -0500

    Update fc, appbase

--- src/main.cpp
+++ src/main.cpp
@@ -30,10 +30,6 @@
 
 using namespace appbase;
 
-namespace fc {
-std::unordered_map<std::string, appender::ptr>& get_appender_map();
-}
-
 namespace detail {
 
 void configure_logging(const bfs::path& config_path) {
@@ -57,29 +53,24 @@ void configure_logging(const bfs::path& config_path) {
 
 } // namespace detail
 
-void logging_conf_loop() {
-    std::shared_ptr<boost::asio::signal_set> sighup_set(new boost::asio::signal_set(app().get_io_service(), SIGHUP));
-    sighup_set->async_wait([sighup_set](const boost::system::error_code& err, int /*num*/) {
-        if (!err) {
-            ilog("Received HUP.  Reloading logging configuration.");
-            auto config_path = app().get_logging_conf();
-            if (fc::exists(config_path))
-                ::detail::configure_logging(config_path);
-            for (auto iter : fc::get_appender_map())
-                iter.second->initialize(app().get_io_service());
-            logging_conf_loop();
-        }
-    });
+void logging_conf_handler() {
+    auto config_path = app().get_logging_conf();
+    if (fc::exists(config_path)) {
+        ilog("Received HUP.  Reloading logging configuration from ${p}.", ("p", config_path.string()));
+    } else {
+        ilog("Received HUP.  No log config found at ${p}, setting to default.", ("p", config_path.string()));
+    }
+    ::detail::configure_logging(config_path);
+    fc::log_config::initialize_appenders(app().get_io_service());
 }
 
 void initialize_logging() {
     auto config_path = app().get_logging_conf();
     if (fc::exists(config_path))
         fc::configure_logging(config_path); // intentionally allowing exceptions to escape
-    for (auto iter : fc::get_appender_map())
-        iter.second->initialize(app().get_io_service());
+    fc::log_config::initialize_appenders(app().get_io_service());
 
-    logging_conf_loop();
+    app().set_sighup_callback(logging_conf_handler);
 }
 
 enum return_codes {

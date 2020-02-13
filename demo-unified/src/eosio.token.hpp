fb20ec3cd84361bba57c85bbde41d56e11078655
Todd Fleming <todd.fleming@block.one>
2020-02-13 17:37:15 -0500

    token example

--- demo-unified/src/eosio.token.hpp
+++ demo-unified/src/eosio.token.hpp
@@ -2,53 +2,63 @@
 
 #include <eosio/asset.hpp>
 #include <eosio/eosio.hpp>
-#include <eosio/unified_lib.hpp>
 #include <string>
 
-class token : public eosio::contract {
-  public:
-    using contract::contract;
+namespace eosio {
 
-    void create(const eosio::name& issuer, const eosio::asset& maximum_supply);
+using std::string;
 
-    void issue(const eosio::name& to, const eosio::asset& quantity, const std::string& memo);
+class [[eosio::contract("eosio.token")]] token : public contract {
+ public:
+   using contract::contract;
 
-    void retire(const eosio::asset& quantity, const std::string& memo);
+   [[eosio::action]] void create(const name& issuer, const asset& maximum_supply);
+   [[eosio::action]] void issue(const name& to, const asset& quantity, const string& memo);
+   [[eosio::action]] void retire(const asset& quantity, const string& memo);
+   [[eosio::action]] void transfer(const name& from, const name& to, const asset& quantity, const string& memo);
+   [[eosio::action]] void open(const name& owner, const symbol& symbol, const name& ram_payer);
+   [[eosio::action]] void close(const name& owner, const symbol& symbol);
+   [[eosio::action]] std::vector<eosio::asset> gettoks(const name& owner);
 
-    void transfer(const eosio::name& from, const eosio::name& to, const eosio::asset& quantity, const std::string& memo);
+   static asset get_supply(const name& token_contract_account, const symbol_code& sym_code) {
+      stats       statstable(token_contract_account, sym_code.raw());
+      const auto& st = statstable.get(sym_code.raw());
+      return st.supply;
+   }
 
-    void open(const eosio::name& owner, const eosio::symbol& symbol, const eosio::name& ram_payer);
+   static asset get_balance(const name& token_contract_account, const name& owner, const symbol_code& sym_code) {
+      accounts    accountstable(token_contract_account, owner.value);
+      const auto& ac = accountstable.get(sym_code.raw());
+      return ac.balance;
+   }
 
-    void close(const eosio::name& owner, const eosio::symbol& symbol);
+   using create_action   = eosio::action_wrapper<"create"_n, &token::create>;
+   using issue_action    = eosio::action_wrapper<"issue"_n, &token::issue>;
+   using retire_action   = eosio::action_wrapper<"retire"_n, &token::retire>;
+   using transfer_action = eosio::action_wrapper<"transfer"_n, &token::transfer>;
+   using open_action     = eosio::action_wrapper<"open"_n, &token::open>;
+   using close_action    = eosio::action_wrapper<"close"_n, &token::close>;
 
-    std::vector<eosio::asset> gettoks(eosio::name owner) {
-        accounts                  accounts(get_self(), owner.value);
-        std::vector<eosio::asset> result;
-        for (auto& acc : accounts)
-            result.push_back(acc.balance);
-        return result;
-    }
+ private:
+   struct [[eosio::table]] account {
+      asset balance;
 
-    struct account {
-        eosio::asset balance;
+      uint64_t primary_key() const { return balance.symbol.code().raw(); }
+   };
 
-        uint64_t primary_key() const { return balance.symbol.code().raw(); }
-    };
+   struct [[eosio::table]] currency_stats {
+      asset supply;
+      asset max_supply;
+      name  issuer;
 
-    struct currency_stats {
-        eosio::asset supply;
-        eosio::asset max_supply;
-        eosio::name  issuer;
+      uint64_t primary_key() const { return supply.symbol.code().raw(); }
+   };
 
-        uint64_t primary_key() const { return supply.symbol.code().raw(); }
-    };
+   typedef eosio::multi_index<"accounts"_n, account>    accounts;
+   typedef eosio::multi_index<"stat"_n, currency_stats> stats;
 
-    typedef eosio::multi_index<"accounts"_n, account>    accounts;
-    typedef eosio::multi_index<"stat"_n, currency_stats> stats;
-
-    void sub_balance(const eosio::name& owner, const eosio::asset& value);
-    void add_balance(const eosio::name& owner, const eosio::asset& value, const eosio::name& ram_payer);
+   void sub_balance(const name& owner, const asset& value);
+   void add_balance(const name& owner, const asset& value, const name& ram_payer);
 };
 
-CONTRACT_ACTIONS(token, create, issue, retire, transfer, open, close)
-CONTRACT_QUERIES(token, gettoks)
+} // namespace eosio

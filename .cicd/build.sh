c9b946ae877fdf615a69da7e3941b7f75aa6101a
Zach Butler <zach.butler@block.one>
2020-04-22 20:38:55 -0400

    I guess "docker run" can't take relative paths

--- .cicd/build.sh
+++ .cicd/build.sh
@@ -8,7 +8,7 @@ docker build -f ubuntu-18.04.dockerfile -t "$IMAGE" .
 echo '+++ :package: Create rodeos Installer'
 [[ -z "$PACKAGE_REVISION" ]] && export PACKAGE_REVISION='1'
 [[ -z "$VERSION_STRING" ]] && export VERSION_STRING="$(docker run -it "$IMAGE" rodeos --version || : | sed 's/^v//' | sed "s/$/-$PACKAGE_REVISION/")"
-echo "$ docker run -e BUILDKITE -e PACKAGE_REVISION -e VERSION_STRING -w /eosio.cdt/libraries/history-tools -it \"$IMAGE\" ./scripts/package-rodeos.sh | tar -xC rodeos_$VERSION_STRING.deb"
-docker run -e BUILDKITE -e PACKAGE_REVISION -e VERSION_STRING -w /eosio.cdt/libraries/history-tools -it "$IMAGE" ./scripts/package-rodeos.sh | tar -xC rodeos_$VERSION_STRING.deb
+echo "$ docker run -e BUILDKITE -e PACKAGE_REVISION -e VERSION_STRING -w /eosio.cdt/libraries/history-tools -it \"$IMAGE\" /eosio.cdt/libraries/history-tools/scripts/package-rodeos.sh | tar -xC rodeos_$VERSION_STRING.deb"
+docker run -e BUILDKITE -e PACKAGE_REVISION -e VERSION_STRING -w /eosio.cdt/libraries/history-tools -it "$IMAGE" /eosio.cdt/libraries/history-tools/scripts/package-rodeos.sh | tar -xC rodeos_$VERSION_STRING.deb
 echo '+++ :arrow_up: Uploading Artifacts'
 buildkite-agent artifact upload '*.deb' 's3://rodeos-binaries/ci'

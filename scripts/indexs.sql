1051c85d16da815c5c3a9ca3c807560fd450ac45
Shupeng Wang <shupeng.wang@block.one>
2020-05-11 11:10:27 +0800

    update

--- scripts/indexs.sql
+++ scripts/indexs.sql
@@ -1,9 +1,9 @@
 /*action_trace table index*/
 CREATE INDEX IF NOT EXISTS index_action_trace_sequence ON action_trace ( sequence );
-CREATE INDEX IF NOT EXISTS index_action_trace_contract_name ON action_trace USING hash ( act_account);
-CREATE INDEX IF NOT EXISTS index_action_trace_action_name ON action_trace USING hash ( act_name);
+CREATE INDEX IF NOT EXISTS index_action_trace_contract_name ON action_trace ( act_account);
+CREATE INDEX IF NOT EXISTS index_action_trace_action_name ON action_trace ( act_name);
 
 
 /*transfer_t index*/
-CREATE INDEX IF NOT EXISTS index_transfer_t_token_from ON transfer_t USING hash (token_from);
-CREATE INDEX IF NOT EXISTS index_transfer_t_token_to ON transfer_t USING hash (token_to);
+CREATE INDEX IF NOT EXISTS index_transfer_t_token_from ON transfer_t (token_from);
+CREATE INDEX IF NOT EXISTS index_transfer_t_token_to ON transfer_t (token_to);

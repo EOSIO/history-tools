6579651ee6798ccdc5ae34e6a2db6e4eb79cb01c
Shupeng Wang <shupeng.wang@block.one>
2020-05-20 11:58:50 +0800

    update statistic

--- scripts/statistic.py
+++ scripts/statistic.py
@@ -39,6 +39,17 @@ class eospg:
         colnames = [desc[0] for desc in cur.description]
         return pd.DataFrame(rows,columns = colnames)
 
+    def get_action_after(self, start_time = dt.datetime.now() - dt.timedelta(days=1)):
+        cur = self.conn.cursor()
+        cur.execute("select block_num from %s.block_info where timestamp >= '%s' order by block_num limit 1" % (self.schema,start_time))
+        rows = cur.fetchall()
+        if(len(rows) == 0):return None
+        start_block = rows[0][0]
+        cur.execute("select * from %s.action_trace where block_num >= '%s'" % (self.schema,start_block))
+        rows = cur.fetchall()
+        colnames = [desc[0] for desc in cur.description]
+        return pd.DataFrame(rows,columns = colnames)
+
 
 
 def main():
@@ -58,16 +69,20 @@ def main():
 
 
     pg = eospg(args.db,args.schema)
-    df_blocks = pg.get_blocks_after(s_time)
-    assert(df_blocks is not None)
-    df_trxs = pg.get_transactions_after(s_time)
-    assert(df_trxs is not None)
-    
-
-
-
-    print(df_trxs.sort_values(by=["cpu_usage_us"]).head(10))
-
+    # df_blocks = pg.get_blocks_after(s_time)
+    # assert(df_blocks is not None)
+    # df_trxs = pg.get_transactions_after(s_time)
+    # assert(df_trxs is not None)
+    df_action = pg.get_action_after(s_time)
+    assert(df_action is not None)
+    # print(df_action[df_action['act_account'] == "addressbook"])
+    print("start from %s to now:" % s_time)
+    print()
+    print("top active contract:")
+    print(df_action.groupby(["act_account"]).size().sort_values(ascending=False).head(5).to_string())
+    print()
+    print("top active account")
+    print(df_action.groupby(["actor"]).size().sort_values(ascending=False).head(5).to_string())
     
     return 0
 

7dedc41253822f62ba76fa27f249b2a06373b39d
Victor Camacho <victor.camacho@block.one>
2021-01-28 07:33:40 -0500

    trying some experiments

--- .cicd/run_python_script.sh
+++ .cicd/run_python_script.sh
@@ -2,4 +2,6 @@ cd .cicd/helpers/
 pip install configparser
 pip install requests
 python2.7 get_eos.py
-cp -a ./builds/current/programs/. /root/history-tools/programs
+echo $PWD
+cp -a $PWD/builds/current/programs/. /root/history-tools/programs
+echo /root/history-tools/programs/nodeos/nodeos --version

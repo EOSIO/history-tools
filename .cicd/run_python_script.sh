ccb3df9fb45c92edc2cf1ba6246a67f6c8c3331c
Scott Arnette <scott.a.arnette@gmail.com>
2021-02-01 12:55:49 -0500

    Update apt first.

--- .cicd/run_python_script.sh
+++ .cicd/run_python_script.sh
@@ -4,10 +4,6 @@ pip install requests
 python2.7 get_eos.py
 echo $PWD
 ls -lash
+apt-get update
 apt install -y $PWD/eosio.deb
 nodeos --version
-# mkdir /root/history-tools
-# mkdir /root/history-tools/programs
-# cp -a $PWD/builds/current/build/programs/. /root/history-tools/programs
-# OUTPUT=`/root/history-tools/programs/nodeos/nodeos --version`
-# echo $OUTPUT

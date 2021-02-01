74d08f931caf9d4ab6a80cc1d210378b4ba02b76
Scott Arnette <scott.a.arnette@gmail.com>
2021-02-01 11:53:01 -0500

    Try installing it.

--- .cicd/run_python_script.sh
+++ .cicd/run_python_script.sh
@@ -4,6 +4,8 @@ pip install requests
 python2.7 get_eos.py
 echo $PWD
 ls -lash
+apt install -y $PWD/eosio.deb
+nodeos --version
 # mkdir /root/history-tools
 # mkdir /root/history-tools/programs
 # cp -a $PWD/builds/current/build/programs/. /root/history-tools/programs
